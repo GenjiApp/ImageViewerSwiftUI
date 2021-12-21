9553080455a63ec53cce2b177b9eaa1dc4e1dcce
Genji <chaos.nonlinear@gmail.com>
2021-12-21 12:07:51 +0900

    make menu commands disabled when images are not opened.

--- Shared/ZoomCommands.swift
+++ Shared/ZoomCommands.swift
@@ -17,16 +17,19 @@ struct ZoomCommands: Commands {
         document?.resetViewSize(animate: true)
       }
       .keyboardShortcut(KeyEquivalent("0"))
+      .disabled(document == nil)
 
       Button("Zoom In") {
         document?.scaleViewSize(2.0, animate: true)
       }
       .keyboardShortcut(KeyEquivalent("+"))
+      .disabled(document == nil)
 
       Button("Zoom Out") {
         document?.scaleViewSize(0.5, animate: true)
       }
       .keyboardShortcut(KeyEquivalent("-"))
+      .disabled(document == nil)
 
       Divider()
     }
